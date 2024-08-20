0641b877ba5c2d377be0b2d4389af8097533efba
ismaail <contact@ismaail.com>
2024-08-20 03:20:04 +0100

    Update DB Seeder: creare Board's Buckets

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Domain\Board\Models\Board;
 use Domain\Board\Types\BoardMembership;
+use Domain\Bucket\Models\Bucket;
 use Domain\User\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -19,6 +20,12 @@ class DatabaseSeeder extends Seeder
         Board::factory(5)
             ->hasAttached(User::factory(1), pivot: ['relation' => BoardMembership::Owner], relationship: 'members')
             ->hasAttached(User::factory(2), pivot: ['relation' => BoardMembership::Guest], relationship: 'members')
+            ->has(Bucket::factory(4)->sequence(
+                ['name' => 'Backlog', 'sort' => 1],
+                ['name' => 'To Do', 'sort' => 2],
+                ['name' => 'In Progess', 'sort' => 3],
+                ['name' => 'Done', 'sort' => 4],
+            ))
             ->create();
     }
 }
