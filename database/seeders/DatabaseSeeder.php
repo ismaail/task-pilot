e6c288adb8cced479ac2b7c7166b9de40e9755f7
ismaail <contact@ismaail.com>
2024-08-20 03:21:18 +0100

    Code formatting & cleanup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,9 +12,6 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         Board::factory(5)
--- src/Domain/Board/Models/BoardMember.php
+++ src/Domain/Board/Models/BoardMember.php
@@ -7,6 +7,9 @@ namespace Domain\Board\Models;
 use Domain\Board\Types\BoardMembership;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
+/**
+ * @mixin IdeHelperBoardMember
+ */
 class BoardMember extends Pivot
 {
     protected function casts(): array
