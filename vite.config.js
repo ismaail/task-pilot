447e29b8424224299eb7c699ba341f7813fe2e16
ismaail <contact@ismaail.com>
2024-08-22 21:06:20 +0100

    Add vite and tailwind config files

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { fileURLToPath, URL } from 'node:url';
+
+import { defineConfig } from 'vite';
+import laravel, { refreshPaths } from 'laravel-vite-plugin';
+
+export default defineConfig({
+	plugins: [
+		laravel({
+			input: [
+				'resources/assets/css/app.css',
+				//'resources/assets/js/app.js',
+			],
+			refresh: [
+				...refreshPaths,
+				'app/Filament/**',
+				'app/Forms/Components/**',
+				'app/Livewire/**',
+				'app/Infolists/Components/**',
+				'app/Providers/Filament/**',
+				'app/Tables/Columns/**',
+			],
+		}),
+	],
+	resolve: {
+		alias: {
+			'~': fileURLToPath(new URL('./resources/assets/js', import.meta.url)),
+			'@livewire': fileURLToPath(new URL('./vendor/livewire/livewire/dist', import.meta.url)),
+		}
+	}
+});
