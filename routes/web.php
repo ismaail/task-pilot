4f6d43ef10abe3beb068ccb1540c4df7bcffede6
ismaail <contact@ismaail.com>
2024-08-22 20:48:27 +0100

    Add /boards/:id GET route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\BoardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('boards/{board}', [BoardController::class, 'show'])->name('boards.show');
