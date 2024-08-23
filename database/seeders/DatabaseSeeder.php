42b025484a3808d5361cac36d2003be886892d5f
ismaail <contact@ismaail.com>
2024-08-23 04:11:38 +0100

    Update DB Seeder to create Bucket's Tasks

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Domain\Board\Models\Board;
 use Domain\Board\Types\BoardMembership;
 use Domain\Bucket\Models\Bucket;
+use Domain\Task\Models\Task;
 use Domain\User\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -17,12 +18,14 @@ class DatabaseSeeder extends Seeder
         Board::factory(5)
             ->hasAttached(User::factory(1), pivot: ['relation' => BoardMembership::Owner], relationship: 'members')
             ->hasAttached(User::factory(2), pivot: ['relation' => BoardMembership::Guest], relationship: 'members')
-            ->has(Bucket::factory(4)->sequence(
-                ['name' => 'Backlog', 'sort' => 1],
-                ['name' => 'To Do', 'sort' => 2],
-                ['name' => 'In Progess', 'sort' => 3],
-                ['name' => 'Done', 'sort' => 4],
-            ))
+            ->has(Bucket::factory(4)
+                ->sequence(
+                    ['name' => 'Backlog', 'sort' => 1],
+                    ['name' => 'To Do', 'sort' => 2],
+                    ['name' => 'In Progess', 'sort' => 3],
+                    ['name' => 'Done', 'sort' => 4],
+                )
+                ->has(Task::factory(random_int(3, 10))))
             ->create();
     }
 }
