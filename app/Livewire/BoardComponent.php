76090bdb1a29b0d77eaa7cb951072499b75bd5d6
ismaail <contact@ismaail.com>
2024-08-27 23:09:55 +0100

    Code formatting and cleanup

--- app/Livewire/BoardComponent.php
+++ app/Livewire/BoardComponent.php
@@ -3,13 +3,14 @@
 namespace App\Livewire;
 
 use Domain\Board\Models\Board;
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class BoardComponent extends Component
 {
     public Board $board;
 
-    public function render()
+    public function render(): View
     {
         return view('livewire.board');
     }
