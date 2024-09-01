063615571c74052ad86f5ab411b6b86fba46ad4f
ismaail <contact@ismaail.com>
2024-09-01 23:53:33 +0100

    Add start() & stop() method to Card Livewire Component

--- app/Livewire/Card/CardComponent.php
+++ app/Livewire/Card/CardComponent.php
@@ -4,16 +4,57 @@ declare(strict_types=1);
 
 namespace App\Livewire\Card;
 
+use Domain\Card\Actions\UpdateCurrentCardAction;
+use Domain\Card\DataObjects\CurrentCardDataObject;
 use Domain\Card\Models\Card;
+use Domain\Timelog\Actions\CreateTimelogAction;
 use Illuminate\Contracts\View\View;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
+#[On('$refresh.card.{card.id}')]
 class CardComponent extends Component
 {
     public Card $card;
 
+    public function start(): void
+    {
+        $currentCard = CurrentCardDataObject::makeFromAuthUser();
+
+        UpdateCurrentCardAction::run($this->card);
+
+        if ($currentCard->id) {
+            CreateTimelogAction::run($currentCard);
+        }
+
+        $this->refreshCard($currentCard);
+    }
+
+    public function stop()
+    {
+        $currentCard = CurrentCardDataObject::makeFromAuthUser();
+
+        UpdateCurrentCardAction::run();
+
+        CreateTimelogAction::run($currentCard);
+    }
+
     public function render(): View
     {
         return view('livewire.card.card-component');
+    }
+
+    /**
+     * Dispatch $refresh event.
+     */
+    private function refreshCard(CurrentCardDataObject $currentCard): void
+    {
+        if (! $currentCard->id) {
+            return;
+        }
+
+        $this
+            ->dispatch('$refresh.card.' . $currentCard->id)
+            ->to('card.card-component');
     }
 }
