753633911f99ac9b532e2d0a57429991259757b2
ismaail <contact@ismaail.com>
2024-09-02 03:48:21 +0100

    Add Timelog Model Observer to update sum of Card's Timelogs

---
+++ src/Domain/Timelog/Observers/TimelogObserver.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Domain\Timelog\Observers;
+
+use Domain\Card\Models\Card;
+use Domain\Timelog\Models\Timelog;
+
+class TimelogObserver
+{
+    public function created(Timelog $timelog): void
+    {
+        $sumSeconds = Timelog::query()
+            ->where('card_id', '=', $timelog->card_id)
+            ->sum('elapsed_seconds');
+
+        Card::query()
+            ->where('id', '=', $timelog->card_id)
+            ->update(['spent_seconds' => $sumSeconds]);
+    }
+}
