8737e450d6bf4439cd1ae1e5c6f15a4e7731261d
ismaail <contact@ismaail.com>
2024-09-03 22:22:48 +0100

    Add wire:key to Bucket <div> and change wire:key value for <card.card-component> in board template

--- resources/views/livewire/board.blade.php
+++ resources/views/livewire/board.blade.php
@@ -2,7 +2,7 @@
 	<h1 class="p-4 bg-white/5 rounded text-lg font-semibold text-white">Board: {{ $board->name }}</h1>
 	<div class="flex w-full flex-grow items-start overflow-y-hidden mt-4 space-x-3">
 		@foreach($board->buckets as $bucket)
-			<div class="w-72 p-2 space-y-2 rounded bg-gray-100 max-h-full overflow-y-auto scrollbar">
+			<div class="w-72 p-2 space-y-2 rounded bg-gray-100 max-h-full overflow-y-auto scrollbar" wire:key="{{ 'Bucket::' . $bucket->id }}">
 				<div class="flex items-start justify-between">
 					{{-- Bucket Title + Cards/ count --}}
 					<h2 class="text-base font-semibold">{{ $bucket->name }}<span class="block text-xs text-gray-600">{{ $bucket->cards->count() }} Tasks</span></h2>
@@ -14,7 +14,10 @@
 					>&plus;</button>
 				</div>
 				@forelse($bucket->cards as $card)
-					<livewire:card.card-component :card="$card" wire:key="{{ $card->id }}"></livewire:card.card-component>
+					<livewire:card.card-component
+						:card="$card"
+						wire:key="{{ 'Card::' . $card->id }}"
+					></livewire:card.card-component>
 				@empty
 					{{-- Create First Task Button --}}
 					<button
