bb9e27fdf27308e75962b5eef4b5e364c224b789
ismaail <contact@ismaail.com>
2024-09-03 02:26:29 +0100

    Add column names parameters to relation method buckets() in the Board Model

--- src/Domain/Board/Models/Board.php
+++ src/Domain/Board/Models/Board.php
@@ -49,6 +49,6 @@ class Board extends Model
 
     public function buckets(): HasMany
     {
-        return $this->hasMany(Bucket::class, '');
+        return $this->hasMany(Bucket::class, 'board_id', 'id');
     }
 }
