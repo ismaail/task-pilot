33ebd5c6ab266d0e5a144e5b98cc78f6609cbcc5
ismaail <contact@ismaail.com>
2024-09-09 01:47:08 +0100

    Remove npm package @alpinejs/sort and install @nextapps-be/livewire-sortablejs

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,12 +1,10 @@
 /* global HSDropdown, Alpine */
 
 import 'preline/dist/dropdown';
-import sort from '@alpinejs/sort';
+import '@nextapps-be/livewire-sortablejs'
 
 window.addEventListener('refresh.preline.dropdown', () => {
 	if (! HSDropdown) return;
 
 	setTimeout(() => HSDropdown.autoInit(), 1000);
 });
-
-Alpine.plugin(sort);
