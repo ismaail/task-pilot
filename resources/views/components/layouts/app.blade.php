1751e0b1ea34cc9bda336c08c00809a4f43822fa
ismaail <contact@ismaail.com>
2024-09-21 23:26:59 +0100

    Show Board Title in the page <title> head tag

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Task Pilot</title>
+	<title>{{ isset($title) ? $title . ' - ' . config('app.title') : config('app.title') }}</title>
 	@livewireStyles
 	@vite('resources/assets/css/app.css')
 </head>
--- resources/views/livewire/board/board-component.blade.php
+++ resources/views/livewire/board/board-component.blade.php
@@ -1,3 +1,4 @@
+<x-slot name="title">{{ $board->name }}</x-slot>
 <div class="flex flex-col w-full px-4 pb-4">
 	<h1 class="p-4 bg-white/5 rounded text-lg font-semibold text-white">Board: {{ $board->name }}</h1>
 	{{-- Buckets --}}
