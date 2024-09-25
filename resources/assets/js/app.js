4b119ba9d60e8276aba95439533627109d9e91e1
ismaail <contact@ismaail.com>
2024-09-25 02:35:23 +0100

    Add 'swap-favicon' window.event in app.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,4 +15,9 @@ document.addEventListener('livewire:initialized', () => {
 	window.addEventListener('notice.add', (event) => {
 		Livewire.dispatch('notice', event.detail[0]);
 	});
+
+	const favicon = document.querySelector('link[rel="icon"]');
+	window.addEventListener('swap-favicon', (event) => {
+		favicon.setAttribute('href', event.detail[0].is_busy ? '/favicon_busy.ico' : '/favicon.ico');
+	});
 });
