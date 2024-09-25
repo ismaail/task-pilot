e8b414fdfc51b8b7852d188966f339441d0e061c
ismaail <contact@ismaail.com>
2024-09-25 02:34:53 +0100

    Add <link rel="icon"> to layout/app template

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,6 +5,11 @@
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>{{ isset($title) ? $title . ' - ' . config('app.title') : config('app.title') }}</title>
+	@if (Auth::user() && Auth::user()->current_card_id)
+		<link rel="icon" type="image/x-icon" href="{{ asset('favicon_busy.ico') }}">
+	@else
+		<link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+	@endif
 	@livewireStyles
 	@vite('resources/assets/css/app.css')
 </head>
