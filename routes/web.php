48959d8a3fd214eec2b4669d83979d9b2c85631e
ismaail <contact@ismaail.com>
2024-10-01 19:24:45 +0100

    Code formatting & cleanup

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
-use App\Livewire\Board\BoardComponent;
-use App\Livewire\Board\TimelogsComponent;
+use App\Livewire\Board;
 use App\Livewire\HomeComponent;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('/', HomeComponent::class)->name('home');
-    Route::get('boards/{board}', BoardComponent::class)->name('boards.show');
-    Route::get('boards/{board}/timelogs', TimelogsComponent::class)->name('boards.timelogs');
+
+    Route::get('boards/{board}', Board\BoardComponent::class)->name('boards.show');
+    Route::get('boards/{board}/timelogs', Board\TimelogsComponent::class)->name('boards.timelogs');
 });
