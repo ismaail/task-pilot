e492c89e5165f69f72832b58c177f9f8fbb741f2
ismaail <contact@ismaail.com>
2024-10-01 21:42:54 +0100

    Remove render() method from Timelogs Components

--- app/Livewire/Profile/TimelogsComponent.php
+++ app/Livewire/Profile/TimelogsComponent.php
@@ -4,13 +4,8 @@ declare(strict_types=1);
 
 namespace App\Livewire\Profile;
 
-use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class TimelogsComponent extends Component
 {
-    public function render(): View
-    {
-        return view('livewire.timelogs-component');
-    }
 }
