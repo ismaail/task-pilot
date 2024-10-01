79fd28e7ca32166154ffec87cf807d7108a5d88b
ismaail <contact@ismaail.com>
2024-10-01 19:24:59 +0100

    Add profile/timelogs GET route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Livewire\Board;
 use App\Livewire\HomeComponent;
+use App\Livewire\Profile;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['auth', 'verified']], function () {
@@ -9,4 +10,6 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
 
     Route::get('boards/{board}', Board\BoardComponent::class)->name('boards.show');
     Route::get('boards/{board}/timelogs', Board\TimelogsComponent::class)->name('boards.timelogs');
+
+    Route::get('profile/timelogs', Profile\TimelogsComponent::class)->name('profile.timelogs');
 });
