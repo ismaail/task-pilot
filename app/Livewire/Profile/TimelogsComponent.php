ae35f222ed23504892fee4518b1ef0f3b5dda4e4
ismaail <contact@ismaail.com>
2024-10-02 02:50:39 +0100

    Show multiple (today, week, ...) total ElapsedTime in Profile/Timelogs

--- app/Livewire/Profile/TimelogsComponent.php
+++ app/Livewire/Profile/TimelogsComponent.php
@@ -4,8 +4,23 @@ declare(strict_types=1);
 
 namespace App\Livewire\Profile;
 
+use Carbon\CarbonImmutable;
+use Domain\Timelog\Models\Timelog;
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class TimelogsComponent extends Component
 {
+    public function render(): View
+    {
+        $now = CarbonImmutable::now();
+
+        return view('livewire.profile.timelogs-component', [
+            'today' => Timelog::TotalTimeBetween($now),
+            'yesterday' => Timelog::TotalTimeBetween($now->subDay()),
+            'week' => Timelog::TotalTimeBetween($now->startOfWeek()),
+            'month' => Timelog::TotalTimeBetween($now->startOfMonth()),
+            'last_month' => Timelog::TotalTimeBetween($now->subMonth()->startOfMonth(), $now->subMonth()->endOfMonth()),
+        ]);
+    }
 }
