2e11106bd50193a861ec3d6ec74a2bc688e43d7c
ismaail <contact@ismaail.com>
2024-10-02 21:39:25 +0100

    Refactor CreateTimlog Action to create Separate log for separate day

--- src/Domain/Timelog/Actions/CreateTimelogAction.php
+++ src/Domain/Timelog/Actions/CreateTimelogAction.php
@@ -8,6 +8,7 @@ use Carbon\CarbonImmutable;
 use Domain\Card\DataObjects\CurrentCardDataObject;
 use Domain\Timelog\Models\Timelog;
 use Domain\Timelog\TimelogException;
+use Illuminate\Support\Facades\DB;
 use Lorisleiva\Actions\Concerns\AsAction;
 
 /**
@@ -23,13 +24,32 @@ class CreateTimelogAction
             throw new TimelogException('Cannot create Timelog without empty Card.');
         }
 
-        $now = CarbonImmutable::now();
-        $secondsDiff = $card->startedAt->diffInSeconds($now);
+        $finishedAt = CarbonImmutable::now();
+
+        if ($card->startedAt->isSameDay($finishedAt)) {
+            $this->create($card->id, $card->startedAt, $finishedAt);
+
+            return;
+        }
+
+        if (! $card->startedAt->addDay()->isSameDay($finishedAt)) {
+            throw new TimelogException('Diff between Start and Finish dates is more than 1 day.');
+        }
+
+        DB::transaction(function () use ($card, $finishedAt) {
+            $this->create($card->id, $card->startedAt, $card->startedAt->endOfDay());
+            $this->create($card->id, $finishedAt->startOfDay(), $finishedAt);
+        });
+    }
+
+    private function create(int $cardId, CarbonImmutable $startedAt, CarbonImmutable $finishedAt): void
+    {
+        $secondsDiff = $startedAt->diffInSeconds($finishedAt);
 
         Timelog::create([
-            'card_id' => $card->id,
-            'started_at' => $card->startedAt,
-            'finished_at' => $now,
+            'card_id' => $cardId,
+            'started_at' => $startedAt,
+            'finished_at' => $finishedAt,
             'elapsed_seconds' => round($secondsDiff),
         ]);
     }
