ca8f3d4ff900e7c1b29d4abed4afd72940b576c4
ismaail <contact@ismaail.com>
2024-10-02 04:32:53 +0100

    Update templates to use <x-utils.dropdown-button>

--- resources/views/components/utils/dropdown-button.blade.php
+++ resources/views/components/utils/dropdown-button.blade.php
@@ -1,9 +1,16 @@
+@props([
+	'dark' => false,
+])
+
 <div {{ $attributes->twMerge('hs-dropdown ml-auto relative inline-flex') }}>
 	@isset($icon)
 		{{ $icon }}
 	@else
-		<button class="p-1 hover:bg-white/20 rounded-full">
-			<x-icons.ellipsis-vertical class="size-5 text-white"></x-icons.ellipsis-vertical>
+		<button
+				@class(['p-1 rounded-full', 'hover:bg-white/20' => $dark, 'hover:bg-gray-200' => !$dark])>
+			<x-icons.ellipsis-vertical
+					@class(['size-5', 'text-white' => $dark, 'text-black' => !$dark])
+			></x-icons.ellipsis-vertical>
 		</button>
 	@endisset
 	<div class="hs-dropdown-menu z-10 transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg p-1 space-y-0.5 mt-2 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full" role="menu" aria-orientation="vertical" aria-labelledby="hs-dropdown-default">
--- resources/views/livewire/board/board-component.blade.php
+++ resources/views/livewire/board/board-component.blade.php
@@ -2,17 +2,12 @@
 <div class="flex flex-col w-full px-4 pb-4">
 	<div class="flex p-4 bg-white/5 rounded">
 		<h1 class="text-lg font-semibold text-white bg">Board: {{ $board->name }}</h1>
-		<div class="hs-dropdown ml-auto relative inline-flex">
-			<button class="p-1 hover:bg-white/20 rounded-full">
-				<x-icons.ellipsis-vertical class="size-5 text-white"></x-icons.ellipsis-vertical>
-			</button>
-			<div class="hs-dropdown-menu z-10 transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg p-1 space-y-0.5 mt-2 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full" role="menu" aria-orientation="vertical" aria-labelledby="hs-dropdown-default">
-				<a href="{{ route('boards.timelogs', $board->id) }}"
-					class="dropdown-button">
-					<span>Timelogs</span>
-				</a>
-			</div>
-		</div>
+		<x-utils.dropdown-button :dark="true">
+			<a href="{{ route('boards.timelogs', $board->id) }}"
+				class="dropdown-button">
+				<span>Timelogs</span>
+			</a>
+		</x-utils.dropdown-button>
 	</div>
 	{{-- Buckets --}}
 	<div
