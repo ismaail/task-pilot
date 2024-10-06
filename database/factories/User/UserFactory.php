da194feada823ba4e5d87eb2e51d61c5ff3e922c
ismaail <contact@ismaail.com>
2024-10-06 02:01:09 +0100

    Add 'current_card_id' & 'current_card_at' to UserFactory

--- database/factories/User/UserFactory.php
+++ database/factories/User/UserFactory.php
@@ -32,6 +32,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'current_card_id' => null,
+            'current_card_at' => null,
         ];
     }
 
