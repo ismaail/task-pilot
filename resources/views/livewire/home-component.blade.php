eb8cf3231faebec63b94c5a0d26c2eee8addced7
ismaail <contact@ismaail.com>
2024-12-30 03:11:44 +0100

    [CSS] Fix Board list layout in Home Page

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -3,7 +3,7 @@
 	<h1 class="p-4 bg-white/5 rounded text-lg font-semibold text-white">All Board</h1>
 	<div class="flex w-full flex-grow items-start overflow-y-hidden mt-4 space-x-3">
 		@foreach($boards as $board)
-			<a href="{{ route('boards.show', $board->id) }}" class="flex items-center w-72 p-2 space-y-2 rounded bg-gray-100 hover:bg-white/90 font-semibold">
+			<a href="{{ route('boards.show', $board->id) }}" class="flex basis-48 shrink-0 items-center w-72 p-2 space-y-2 rounded bg-gray-100 hover:bg-white/90 font-semibold">
 				{{ $board->name }}
 				@if ($current_board_id === $board->id)
 					<x-icons.play class="ms-auto size-5 fill-primary text-primary"></x-icons.play>
@@ -13,7 +13,7 @@
 			<button
 					wire:click="$dispatch('openModal', { component: 'board.modals.create-board'})"
 					title="Create new Board"
-					class="flex items-center gap-x-1 w-72 p-2 rounded text-white hover:text-primary font-semibold cursor-pointer">
+					class="flex basis-36 shrink-0 items-center gap-x-1 w-72 p-2 rounded text-white hover:text-primary font-semibold cursor-pointer">
 				<x-icons.plus class="size-5" /> New Board
 			</button>
 	</div>
