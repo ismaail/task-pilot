bbb0437b9739740d0bcfe13091b919a55e5682d4
ismaail <contact@ismaail.com>
2025-11-25 03:10:55 +0100

    Update <x-utils.dropdown-button> view component

--- resources/views/components/utils/dropdown-button.blade.php
+++ resources/views/components/utils/dropdown-button.blade.php
@@ -9,11 +9,13 @@
 		<button
 				@class(['p-1 rounded-full', 'hover:bg-white/20' => $dark, 'hover:bg-gray-200' => !$dark])>
 			<x-icons.ellipsis-vertical
-					@class(['size-5', 'text-white' => $dark, 'text-black' => !$dark])
+					@class(['size-5', 'text-text'])
 			></x-icons.ellipsis-vertical>
 		</button>
 	@endisset
-	<div class="hs-dropdown-menu z-10 transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg p-1 space-y-0.5 mt-2 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full" role="menu" aria-orientation="vertical" aria-labelledby="hs-dropdown-default">
-		{{ $slot }}
-	</div>
+		<div class="hs-dropdown-menu min-w-40 mt-2 px-2 p-1 space-y-0.5 z-10 transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden bg-gray-300 shadow-md rounded border border-gray-700
+		after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full text-text"
+			role="menu" aria-orientation="vertical" aria-labelledby="hs-dropdown-default">
+			{{ $slot }}
+		</div>
 </div>
