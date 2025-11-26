496466dc141856db78e25ee24def7e7d4644f535
ismaail <contact@ismaail.com>
2025-11-26 03:43:58 +0100

    Update Modal styles

--- resources/views/livewire/card/modals/create-card.blade.php
+++ resources/views/livewire/card/modals/create-card.blade.php
@@ -1,25 +1,25 @@
 <div class="w-[600px] py-1">
 	<div class="flex px-2 justify-between items-center">
-		<h2 class="font-semibold">Create New Task</h2>
-		<button wire:click="$dispatch('closeModal')" class="fill-secondary text-white hover:fill-secondary/90 hover:text-white">
+		<h2 class="font-semibold">@lang('Create New Task')</h2>
+		<button wire:click="$dispatch('closeModal')" class="fill-secondary text-text cursor-pointer hover:contrast-75">
 			<x-icons.circle-close class="size-8"></x-icons.circle-close>
 		</button>
 	</div>
 	<form class="my-2 px-2 pt-2 space-y-4 border-t border-gray-300" wire:submit.prevent="create">
 		<div>
-			<label for="name" class="block textsm font-semibold">Name</label>
-			<input type="text" id="name" wire:model="form.name" required autocomplete="name" autofocus class="block w-full mt-2 py-1 px-2 border border-gray-400 rounded-md shadow-sm outline-none focus:border-sky-500 focus:ring-sky-500">
+			<label for="name" class="form-label">@lang('Name')</label>
+			<input type="text" id="name" wire:model="form.name" required autocomplete="name" autofocus class="form-input w-full">
 			@error('form.name')<span class="text-xs text-red-600">{{ $message }}</span>@enderror
 		</div>
 		<div>
-			<label for="description" class="block textsm font-semibold">description</label>
-			<textarea type="text" id="description" wire:model="form.description" rows="3" class="block w-full mt-2 py-1 px-2 border border-gray-400 rounded-md shadow-sm outline-none focus:border-sky-500 focus:ring-sky-500"></textarea>
+			<label for="description" class="form-label">@lang('description')</label>
+			<textarea type="text" id="description" wire:model="form.description" rows="3" class="form-input w-full"></textarea>
 			@error('form.description')<span class="text-xs text-red-600">{{ $message }}</span>@enderror
 		</div>
 		<div class="flex justify-end">
-			<button type="submit" class="floatend w-40 mr-2 px-5 py-2 rounded bg-secondary hover:bg-secondary/90 disabled:bg-gray-400 text-center font-semibold text-white tracking-wider">
-				<span wire:loading.class="hidden">Create</span>
-				<span wire:loading><x-icons.spinner /></span>
+			<button type="submit" class="btn disabled:bg-gray-300">
+				<span wire:loading.class="hidden">@lang('Create')</span>
+				<span wire:loading><x-icons.spinner class="size-5" /></span>
 			</button>
 		</div>
 	</form>
