248c61c9ab2b16ba827ff12b648418819738a842
ismaail <contact@ismaail.com>
2025-12-20 17:23:05 +0100

    Fix: text color in Board Timelogs page

--- resources/views/livewire/board/timelogs-component.blade.php
+++ resources/views/livewire/board/timelogs-component.blade.php
@@ -1,8 +1,8 @@
 <x-slot name="title">{{ $board->name }}</x-slot>
 <div class="flex flex-col w-full px-4 pb-4">
-	<h1 class="p-4 flex items-center justify-between bg-white/5 rounded text-lg font-semibold text-white">
-		Timelogs for: {{ $board->name }}
-		<a href="{{ route('boards.show', $board->id) }}" class="text-white text-sm hover:underline">&#x2B8C; Back</a>
+	<h1 class="p-4 flex items-center justify-between bg-gray-300 rounded text-lg font-semibold text-text">
+		@lang('Timelogs') : {{ $board->name }}
+		<a href="{{ route('boards.show', $board->id) }}" class="text-text text-sm hover:underline"><span class="text-lg">&#x2B8C;</span> @lang('Back')</a>
 	</h1>
 	<main class="mt-4">
 		<x-chart.time-chart-component :board-id="$board->id" />
