c65f181b925184305ffb15ea700ffcc0f12d3f12
ismaail <contact@ismaail.com>
2026-01-20 17:43:31 +0100

    Login Page style fix

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -7,6 +7,37 @@
 	<title>Task Pilot</title>
 	@livewireStyles
 	@vite('resources/assets/css/app.css')
+	<style>[x-cloak] {display: none;}</style>
+	{{-- Dark Mode--}}
+	<script>
+		const changeDarkMode = (isDark) => {
+			globalThis.DarkMode = isDark;
+
+			if (isDark) {
+				document.documentElement.setAttribute('data-mode', 'dark');
+				localStorage.setItem('dark-mode', 'true');
+
+				return;
+			}
+
+			document.documentElement.removeAttribute('data-mode');
+			localStorage.setItem('dark-mode', 'false');
+		};
+
+		window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
+			changeDarkMode(event.matches);
+		});
+
+		const isDark = () => {
+			return null !== localStorage.getItem('dark-mode')
+				? localStorage.getItem('dark-mode') === 'true'
+				:window.matchMedia('(prefers-color-scheme: dark)').matches;
+		};
+
+		changeDarkMode(isDark());
+
+		document.addEventListener('livewire:navigated', () => changeDarkMode(isDark()));
+	</script>
 </head>
 <body>
 <div class="flex flex-grow w-full overflow-y-hidden">
